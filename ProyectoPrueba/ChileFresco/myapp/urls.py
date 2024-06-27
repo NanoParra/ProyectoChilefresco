b06bf70a6682e20143fe7f3e13e4e0473af01130
Esteban Parra <130527260+NanoParra@users.noreply.github.com>
2024-06-26 22:32:08 -0400

    Add files via upload

--- ProyectoPrueba/ChileFresco/myapp/urls.py
+++ ProyectoPrueba/ChileFresco/myapp/urls.py
@@ -3,5 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('bebes/', views.bebes, name='bebes'),
+    path('carro_compra/', views.carro_compra, name='carro_compra'),
+    path('contactanos/', views.contactanos, name='contactanos'),
+    path('cuenta/', views.cuenta, name='cuenta'),
     path('', views.index, name='index'),
+    path('pago/', views.pago, name='pago'),
+    path('panaderia/', views.panaderia, name='panaderia'),
+    path('productos/', views.productos, name='productos'),
+    path('terminos_condiciones/', views.terminos_condiciones, name='terminos_condiciones'),
+    path('verduleria/', views.verduleria, name='verduleria'),
 ]
+    
+
--- ProyectoPrueba/ChileFresco/myapp/views.py
+++ ProyectoPrueba/ChileFresco/myapp/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-from django.shortcuts import render
+# Create your views here
 
 def index(request):
     return render(request, 'myapp/index.html')
+    
+def bebes(request):
+    return render(request, 'productos/BEBECOMESTIBLES.html')
+
+def carro_compra(request):
+    return render(request, 'productos/CARRO_COMPRA.html')
+
+def contactanos(request):
+    return render(request, 'productos/CONTACTANOS.html')
+
+def cuenta(request):
+    return render(request, 'productos/CUENTA.html')
+
+def index(request):
+    return render(request, 'productos/index.html')
+
+def pago(request):
+    return render(request, 'productos/PAGO.html')
+
+def panaderia(request):
+    return render(request, 'productos/PANADERIA.html')
+
+def productos(request):
+    return render(request, 'productos/PRODUCTOS.html')
+
+def terminos_condiciones(request):
+    return render(request, 'productos/TERMINOS_CONDICIONES.html')
+
+def verduleria(request):
+    return render(request, 'productos/VERDULERIA.html')
